57feb9bbf4cd49599f4b827e017b1cd842de3ec8
cjspiece <cjspiece@svsu.edu>
2016-09-07 18:25:23 -0400

    Added additional logic

---
+++ app/src/main/java/com/example/chris/beeradviser/BeerExpert.java
@@ -0,0 +1,22 @@
+package com.example.chris.beeradviser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Chris on 9/7/2016.
+ */
+public class BeerExpert {
+    List<String> getBrands(String color) {
+        List<String> brands = new ArrayList<>();
+        if (color.equals("amber")) {
+            brands.add("Jack Amber");
+            brands.add("Red Moose");
+        }
+        else {
+            brands.add("Jail Pale Ale");
+            brands.add("Gout Stout");
+        }
+        return brands;
+    }
+}
